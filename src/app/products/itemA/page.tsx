2c955170fa98783077d8014732574e361f5f17db
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-03-28 05:39:57 +0900

    add:dynamic-routes

---
+++ src/app/products/itemA/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const page = () => {
+  // 固定ページならOKだが投稿など動的コンテンツには向かない
+  return <div>商品A</div>;
+};
+
+export default page;
---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      <h1>商品一覧</h1>
+      <p>商品A</p>
+      <p>商品B</p>
+      <p>商品C</p>
+    </div>
+  );
+};
+
+export default page;
