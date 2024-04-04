0bba82253bcc1a922a4bf4125570a73bbb81eb11
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-04-04 12:02:29 +0900

    add:catch-segments

---
+++ src/app/news/[[...slug]]/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const page = ({ params }: { params: { slug: string[] } }) => {
+  // [id]などとするとスラッグを動的に変更できるが入れ子構造には対応できない
+  // [[...id]]とすることで入れ子構造の動的なファイルを作成できる　news/2024/0001などidxid分のファイル作成は不要になる
+  // 一度定義すればスラッグの構成に合わせて自動でレンダリングできる
+  if (params.slug?.length === 2) {
+    return (
+      <h1>
+        {params.slug[0]}と{params.slug[1]}です。
+      </h1>
+    );
+  } else if (params.slug?.length === 1) {
+    return <h1>{params.slug[0]}です。</h1>;
+  } else {
+    return <h1>News</h1>;
+  }
+};
+
+export default page;
