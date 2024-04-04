7b6ae39bc736ecd2d0ed2141cc15e56e7e090e29
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-04-04 12:11:54 +0900

    add:custom-404

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = () => {
+  // ファイル名は固定なのでコンポーネント名を変えることに注意
+  // ディレクトリ別に設置することも可能。reviewディレクトリに移動して実践
+  return <div>not-found</div>;
+};
+
+export default NotFound;
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,3 +1,4 @@
+import { notFound } from 'next/navigation';
 import React from 'react';
 // フォルダ名に動的セグメントを持たせることで、ネストされた動的ルートを作成できる
 const page = ({
@@ -9,7 +10,11 @@ const page = ({
   };
 }) => {
   console.log(params);
-
+  // レビューidが1000までしかないので1001以上はページがない想定
+  if (parseInt(params.reviewId) > 1000) {
+    // インポート文も書く
+    notFound();
+  }
   return (
     <div>
       商品ID：{params.productId}のレビューID：{params.reviewId}
