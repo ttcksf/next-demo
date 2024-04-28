e70acc458d594f61f0f17d4dc6c37c4ec6aa94dc
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-04-28 12:05:57 +0900

    add:routes-group

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const page = () => {
+  // 似たような機能はネストしたフォルダにするのが基本
+  // ただURLをネストにしたくない時がある
+  // 親のフォルダを()で囲むと親のフォルダ名をURLから省略できる
+  return <div>会員登録</div>;
+};
+
+export default page;
