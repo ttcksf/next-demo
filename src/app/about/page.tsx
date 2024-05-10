8693b9b2b968c54411802027dc435dddffadcce4
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-05-10 23:26:56 +0900

    add:title-metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,10 @@
+import { Metadata } from 'next';
 import React from 'react';
 
-export const metadata = {
-  title: 'アバウトページです',
+// layout.tsxのオブジェクト形式のタイトルを受け取るのにも型指定とimportが必要
+export const metadata: Metadata = {
+  // title: 'アバウトページです',
+  title: '会社概要',
   description: '説明文です',
 };
 
